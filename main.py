847c3e42ab01c5eb01efb4d589ec91731eb06a71
smilez-detcelfer <detcelfer.tech@pm.me>
2023-03-08 18:28:32 +0300

    feature 4 another commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def fistFunc():
     print('this is function')
     print('changed in master')
     print('feature 4 changes in main')
+    print('another feature 4 changes')
 
 def fuctionfromBranch():
     print('this is function from branch')
