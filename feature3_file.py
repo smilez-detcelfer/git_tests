01f00a8fabff1eeb3aded97039fdd5e02a97764d
smilez-detcelfer <detcelfer.tech@pm.me>
2023-03-08 18:57:54 +0300

    master commit paralell with feature 5

--- feature3_file.py
+++
@@ -1,2 +0,0 @@
-def feature3():
-    print('feature3')
--- main.py
+++ main.py
@@ -1,15 +1,3 @@
 def fistFunc():
     print('this is function')
     print('in feature 5 deleted unnecessary strings')
-
-def feature5():
-    print('another feature5')
-
-def anotherfunc():
-    print('master updated')
-
-def masterfunc():
-    print('master added before feature 4 merged ')
-
-def masterfeature5parallel():
-    print('this should be rebased to feature 5')
