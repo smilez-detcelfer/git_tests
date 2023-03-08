0f614e19b82ac4ea4f85baec730b8260f448cb0e
smilez-detcelfer <detcelfer.tech@pm.me>
2023-03-08 19:17:40 +0300

    two first functions done

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ def fistFunc():
     print('this is function')
     print('in feature 5 deleted unnecessary strings')
 
+
 def secondfunc():
-    print('123')
-
-
-def masterfunc():
-    print('from master')
+    print('feature from branch')
