d3cd5c025287cbbe57208aaedca7d78d74a733ba
smilez-detcelfer <detcelfer.tech@pm.me>
2023-03-08 17:21:06 +0300

    feature 4 added file and make changes in main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def fistFunc():
     print('this is function')
     print('changed in master')
+    print('feature 4 changes in main')
 
 def fuctionfromBranch():
     print('this is function from branch')
