a29ea3df2d3e1769520d9e43fe89cc8255868cda
smilez-detcelfer <detcelfer.tech@pm.me>
2023-03-09 23:00:31 +0300

    saved master

--- main.py
+++ main.py
@@ -3,14 +3,3 @@ def fistFunc():
     print('changed in master to new')
     print('new changes in master')
 
-
-def featurefunc():
-    print('feature from new branch')
-
-def anotherfuncfrombranch():
-    print('new changed from branch')
-
-#some changes
-
-
-#another fucking branch code
